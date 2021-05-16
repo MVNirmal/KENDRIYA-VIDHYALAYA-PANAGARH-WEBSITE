555543e69ed56900f6641d281fa8ff24476cfc96
MVNirmal <81420492+MVNirmal@users.noreply.github.com>
2021-05-16 20:38:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function establish() {
+
+    document.getElementById("b7").innerHTML = "1996";
+
+}
+
+
+function manage() {
+
+    document.getElementById("b2").innerHTML = "Defence";
+
+}
+
+function gender() {
+
+    document.getElementById("b3").innerHTML = "CO-Education (Both Boys and Girls)";
+
+}
+
+function boarding() {
+
+    document.getElementById("b4").innerHTML = "Day School";
+
+}
+
+function grades() {
+    
+   document.getElementById("b5").innerHTML="From class 1st to 12th";
+   
+}
+
+function curriculum() {
+    
+   document.getElementById("b6").innerHTML="Central Board of Secondary Education (CBSE)";
+   
+}
